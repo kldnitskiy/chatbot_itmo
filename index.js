31b093a7abc34b78e65df0c6cce2c8138681d6ae
Danl Kladnitskiy <core2pixel@gmail.com>
2020-03-31 21:00:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new Botact({
 })
 
 bot.on(function (user_message){
-    console.log(user_message.body)
+    console.log(user_message.id)
     user_message.reply('Привет')
 })
 bot.event('group_join', ({ reply }) => {
