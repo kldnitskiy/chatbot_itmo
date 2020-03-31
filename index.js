9f30a109a32ba038fd2983c02a93fd46e9ab8f76
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 01:34:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ SetPair = function(callback, id, pair_id, user_message) {
         return; 
       }
         //return results;
+        bot.reply(pair, msg);
       callback(false, results, id);
     });
   });
@@ -171,7 +172,7 @@ function saveUsers(status, result){
 }
 let pair;
 function savePair(status, result, pair){
-bot.reply(pair, 'Собеседник найден! Устанавливаем соединение...');
+
 }
 let unpaired = getUnPaired(saveUnpaired);
 function saveUnpaired(status, result){
