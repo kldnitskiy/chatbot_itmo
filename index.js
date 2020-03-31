c0376b8b339a3f8997134f28818a67c96a00791e
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 01:20:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ getPair = function(callback, id) {
       callback(true); 
       return; 
     }
-    let sql = "SELECT vk_id FROM chatbot_data WHERE vk_id = "+id+"";
+    let sql = "SELECT vk_id FROM chatbot_data WHERE pair_id = "+id+"";
     connection.query(sql, [], function(err, results) {
       connection.release(); // always put connection back in pool after last query
       if(err) { 
