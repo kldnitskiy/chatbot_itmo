f489ee6c93989667c67c8eca420ed4397fec9bcc
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 15:20:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//MODULES
 const express = require('express')
 const {Botact} = require('botact')
 const bodyParser = require('body-parser')
@@ -5,12 +6,14 @@ const mysql = require('mysql')
 const server = express()
 const PORT = process.env.PORT || 80
 
-server.listen(PORT)
+
 //BOT SETUP
 const bot = new Botact({
     token: '91c66aec637d3f1ea0615132ea568793ffc55b18c0dd878c386584ee226bf7264f669f30cc61986c8e3e2',
     confirmation: 'f5f07863'
 })
+
+
 //SERVER SETUP
 server.use(bodyParser.json())
 server.post('/', bot.listen)
@@ -18,46 +21,24 @@ server.get('/', (request, response) => {
     request.header('Content-Type', 'application/json')
     response.send('f5f07863')
 })
-//MYSQL SETUP
-//let con = mysql.createConnection({
-//  host: "us-cdbr-iron-east-01.cleardb.net",
-//  user: "b09805f711cdac",
-//  password: "c362ba82",
-//database: "heroku_2cf38b0299dd81c"
-//});
-//
-//con.connect(function(err) {
-//  if (err) throw err;
-//  console.log("Connected to MYSQL!");
-//});
+server.listen(PORT)
 
-//Вставить id человека
-//function insert_vk_id(vk_id){
-//  let sql = "INSERT INTO chatbot_data (vk_id) VALUES ("+vk_id+")";
-//    let check_user = "SELECT vk_id FROM chatbot_data WHERE vk_id = "+vk_id+" ";
-//  con.query(check_user, function (err, result) {
-//    if (err) throw err;
-//      if(typeof(result) === 'object' && Object.keys(result).length === 0){
-//          con.query(sql, function (err, result) {
-//    if (err) throw err;
-//              console.log('Inserted new vk_id');
-//              con.end(function(err) {});
-//  }); 
-//      }
-//  });  
-//    
-//}
-let bot_control;
-var pool  = mysql.createPool({
+
+//MYSQL SETUP
+let pool  = mysql.createPool({
 host: "us-cdbr-iron-east-01.cleardb.net",
 user: "b09805f711cdac",
 password: "c362ba82",
 database: "heroku_2cf38b0299dd81c"
 });
 
-let msg;
-let connected = false;
-let partner_id = 0;
+
+//API
+let msg; //Текущее сообщение
+let connected = false; //Находится в чате
+let partner_id = 0; //Id текущей пары
+let pair; //Id текущей пары
+
 //getUsers
 getUsers = function(callback) {
   pool.getConnection(function(err, connection) {
@@ -79,6 +60,9 @@ getUsers = function(callback) {
     });
   });
 };
+
+
+//getPair
 getPair = function(callback, id, msg) {
   pool.getConnection(function(err, connection) {
     if(err) { 
@@ -94,8 +78,6 @@ getPair = function(callback, id, msg) {
         callback(true); 
         return; 
       }
-        //return results;
-
       pair = results[0].vk_id;  
         partner_id = pair;
       callback(false, results, pair, msg);
@@ -103,6 +85,8 @@ getPair = function(callback, id, msg) {
     });
   });
 };
+
+
 //getUnpaired
 getUnPaired = function(callback) {
   pool.getConnection(function(err, connection) {
@@ -124,6 +108,8 @@ getUnPaired = function(callback) {
     });
   });
 };
+
+
 //CheckPair
 CheckPair = function(callback, id, user_message) {
     console.log('Запрос от ' + id)
@@ -146,6 +132,8 @@ CheckPair = function(callback, id, user_message) {
     });
   });
 };
+
+
 //SetPair
 SetPair = function(callback, id, pair_id) {
     console.log('SetPair');
@@ -171,17 +159,21 @@ SetPair = function(callback, id, pair_id) {
   });
 };
 
+
+//CALLBACKS
 let users = getUsers(saveUsers);
 function saveUsers(status, result){
     users = result;
+    console.log('Всего пользователей');
     console.log(users);
 }
-let pair;
+
 function savePair(status, result, pair){
 }
 let unpaired = getUnPaired(saveUnpaired);
 function saveUnpaired(status, result){
     unpaired = result;
+    console.log('Всего без пары');
     console.log(unpaired);
 }
 function savePaired(status, result, id, pair_id){
@@ -292,46 +284,45 @@ function saveMember(status, results, id){
     
 }
 //saveNewMember(saveMember, reply.vk_id);
+
+
 //BOT BASIC EVENTS
+
+//Стандартные сообщения
 let group_join_msg = 'Добро пожаловать в семью!Ты сделал маленький, но весомый шаг к незабываемой студенческой жизни♥Это особенное место с неповторимыми людьми✨ А ты уже - наша часть 😌Мы обещаем, будет интересно 😏. \nНапишите команду start, чтобы присоединиться к анонимной беседе студентов.';
 let group_leave_msg = 'Жаль, что покидаете Нас((';
+
+//Вступление в группу
 bot.event('group_join', ({ reply }) => {
   reply(group_join_msg)
     //saveNewMember(saveMember, reply.vk_id);
 })
+//Выход из группы
 bot.event('group_leave', ({ reply }) => {
   reply(group_leave_msg)
 })
 
 
+
+//Начать беседу + найти пару
 bot.command('start', (ctx) => {
-    saveNewMember(saveMember, parseInt(ctx.user_id));
+    //saveNewMember(saveMember, parseInt(ctx.user_id));
     //console.log(ctx.user_id);
 })
-bot.command('/start', (ctx) => {
-    saveNewMember(saveMember, parseInt(ctx.user_id));
-    //console.log(ctx.user_id);
-})
+//начать беседу
 bot.command('go', (ctx) => {
     ctx.reply('Вы подключены к чату');
     //console.log(ctx.user_id);
 })
-bot.command('/go', (ctx) => {
-    ctx.reply('Вы подключены к чату');
-    //console.log(ctx.user_id);
-})
-bot.command('/exit', (ctx) => {
-    ctx.reply('Завершаем сеанс. Напишите команду /start, чтобы начать новую беседу.');
-    closeSession(Session, parseInt(ctx.user_id));
-})
+//выйти из беседы
 bot.command('exit', (ctx) => {
-    ctx.reply('Завершаем сеанс. Напишите команду /start, чтобы начать новую беседу.');
-    closeSession(Session, parseInt(ctx.user_id));
+    ctx.reply('Завершаем сеанс. Напишите команду /start, чтобы найти нового собеседника.');
+    //closeSession(Session, parseInt(ctx.user_id));
 })
 
 //BOT REPLIES
+/*
     bot.on(function (user_message){
-        
     msg = user_message.body;
     console.log(user_message.user_id + 'Отправлено к: ' +partner_id);
     console.log(user_message.user_id + ':'+msg);
@@ -350,12 +341,9 @@ bot.command('exit', (ctx) => {
         }
         }
         let isUnpaired;
-            
-   
-        
 })
 
-
+*/
 
 
 //mysql://b09805f711cdac:c362ba82@us-cdbr-iron-east-01.cleardb.net/heroku_2cf38b0299dd81c?reconnect=true
