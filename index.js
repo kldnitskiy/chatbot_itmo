243b333b59b453485f96dcb5eb6eea4ada7c7986
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 23:17:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -212,8 +212,11 @@ noticeCloseSession = function(callback) {
         console.log(err); 
         return; 
       }
-        bot.reply(parseInt(results[0].vk_id), 'Ваш собеседник вышел из чата. Напишите команду start, чтобы начать новую беседу')
+        if(Object.keys(results).length>0){
+            bot.reply(parseInt(results[0].vk_id), 'Ваш собеседник вышел из чата. Напишите команду start, чтобы начать новую беседу')
         callback()
+        }
+        
     });
   });
 };
