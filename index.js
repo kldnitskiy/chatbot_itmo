30a92abaf962efbf8c1b97d3dc661658386477fb
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 12:51:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -217,14 +217,12 @@ function checkifUnpaired(status, result, user_message){
 let current_message;
 //BOT REPLIES
     bot.on(function (user_message){
+        console.log(connected);
         bot_control = user_message;
     msg = user_message.body;
     console.log(user_message.user_id + ':'+msg);
             let isUnpaired = CheckPair(checkifUnpaired, user_message.user_id,user_message); 
-    
-        if(connected){
-            bot.reply(parseInt(pair), 'Собеседник: ' +msg);
-        }
+   // bot.reply(parseInt(pair), 'Собеседник: ' +msg);
         
 })
 
