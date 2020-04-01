0fe80932252a9b968d07bdc51f701274569651ba
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-01 23:10:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ closeSession = function() {
       console.log(err); 
       return; 
     }
-    let sql = "UPDATE chatbot_data SET (pair_id) VALUES('') WHERE pair_id = "+user_id+"";
+    let sql = "UPDATE chatbot_data SET pair_id = (case when pair_id = "+user_id+" then  '' end)";
     connection.query(sql, [], function(err, results) {
       connection.release(); // always put connection back in pool after last query
       if(err) { 
