42c50e3a469036ffef79285e3aa3f8cf98ace819
kladnitskiy <31546027+kldnitskiy@users.noreply.github.com>
2020-04-02 16:34:17 +0300

    Add files via upload

---
+++ db_test.js
@@ -0,0 +1,18 @@
+var express    = require("express");
+ var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '1812danil',
+   database : 'chatbot_data'
+ });
+ var app = express();
+ 
+ connection.connect(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");  
+ } else {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });
+app.listen(3000);
