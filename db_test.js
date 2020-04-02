f0666dac8e210584af02c2d80b814750993ecbab
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-02 16:37:27 +0300

    db_change

--- db_test.js
+++ db_test.js
@@ -1,7 +1,7 @@
 var express    = require("express");
  var mysql      = require('mysql');
  var connection = mysql.createConnection({
-   host     : 'localhost',
+   host     : '127.0.0.1',
    user     : 'root',
    password : '1812danil',
    database : 'chatbot_data'
