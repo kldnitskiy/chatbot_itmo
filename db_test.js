a46a40c0bd97fa624c21750d937093263d66e1f4
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-03 21:23:24 +0300

    Update db_test.js

--- db_test.js
+++ db_test.js
@@ -1,43 +1,19 @@
-
-var Client = require('mysql').Client;
-var client = new Client(); 
-client.host ='localhost';
-client.user = 'root';
-client.password = '1812danil';
-console.log("connecting...");
-client.connect(function(err, results) {
-    if (err) {
-        console.log("ERROR: " + err.message);
-        throw err;
-    }
-    console.log("connected.");
-    clientConnected(client);
-});
-
-clientConnected = function(client)
-{
-	tableHasData(client);
-}           
-
-
-tableHasData = function(client)
-{
-    client.query(
-        'SELECT * FROM db.table LIMIT 0,10',
-        function selectCb(err, results, fields) {
-            if (err) {
-                console.log("ERROR: " + err.message);
-                throw err;
-            }
-            console.log("Got "+results.length+" Rows:");
-            for(var i in results){
-			 
-				//console.log(results[i].[column name]); 
-				console.log('\n');
-				
-            //console.log("The meta data about the columns:");
-            //console.log(fields);     
-			}
-            client.end();
-        });
-};
+var express    = require("express");
+ var mysql      = require('mysql');
+ var pool = mysql.createPool({
+   host     : 'localhost',
+   user     : 'root',
+   password : '1812danil',
+   database : 'chatbot_data',
+     port: '8888'
+ });
+ var app = express();
+ 
+pool.getConnection(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");  
+ } else {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });
+app.listen(3000);
