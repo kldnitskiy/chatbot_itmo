c21d45e3ae9ca6993573a245a8c8bc1a62cc2265
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-05 22:11:14 +0300

    Update callback.js

--- callback.js
+++ callback.js
@@ -22,13 +22,12 @@ let btnSearch = {
 }
 module.exports = {
   updateUserInfo: function (status, result, user_id) {
-      console.log(result);
       //Проверяем статус запроса
       if(!status){
           bot.reply(user_id, 'Произошла ошибка на сервере. Попробуйте позже.');
       }
     if(Object.keys(result).length === 0){
-        bot.reply(user_id, 'Вижу, тебя нет в базе. Введите команду search, чтобы найти собеседника.', null, btnSearch);
+        bot.reply(user_id, 'Вижу, тебя нет в базе. Введите команду search, чтобы найти собеседника.');
     }else if(status){
         pair_id = result[0].pair_id;
         bot.reply(user_id, 'У вас есть активный чат. Напишите join, чтобы войти в него.');
