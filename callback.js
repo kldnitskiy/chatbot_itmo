f45b875b2a7ae5e040ed1d0149bc715cf31ee435
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-05 21:32:14 +0300

    api dependecies

--- callback.js
+++ callback.js
@@ -1,4 +1,10 @@
 const mysql = require('mysql')
+let pool  = mysql.createPool({
+   host     : "localhost",
+   user     : "root",
+   password : "1812danil",
+   database : "chatbot_itmo"
+});
 module.exports = {
 
   updateUserInfo: function (status, result) {
