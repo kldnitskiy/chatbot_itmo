d39e1ad4e80916d763bfcd7fd6c12fab032f5422
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-06 11:31:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ let users_data = {
 console.log('running')
 
 bot.on(function (res){
-    console.log(users_data['users'])
+    users_data['users'].id = res.user_id;
+    console.log(user_data)
     console.log(res.user_id + ' Написал: ' + res.body)
     //Убираем клавиатуру
 })
