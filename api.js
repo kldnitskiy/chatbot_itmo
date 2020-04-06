bcd1c3b2511db7163be6bdfafd3b9f6088781ca3
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-06 12:52:26 +0300

    Update api.js

--- api.js
+++ api.js
@@ -46,7 +46,7 @@ module.exports = {
                 callback(false);
                 return;
             }
-            let sql = "SELECT vk_id FROM chatbot_data WHERE pair_id IS NULL AND vk_id != "+user_id+" ";
+            let sql = "SELECT vk_id FROM chatbot_data WHERE pair_id IS NULL AND vk_id != "+user_id+";UPDATE chatbot_data SET pair_id = "+user_id+" WHERE pair_id IS NULL AND vk_id != "+user_id+";";
             connection.query(sql, [], function (err, result) {
                 connection.release(); // always put connection back in pool after last query
                 if (err) {
