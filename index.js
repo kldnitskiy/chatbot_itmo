2d7f3033ce0ff3481d66aae6db438b650ebbafa3
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-07 13:06:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ bot.command('Exit', (res) => {
      api.noticeExit(callback.requestManager, res.user_id, null);
 })
 bot.on(function (res){
+    console.log(res)
     console.log(res.user_id + ' написал: ' + res.body)
     api.isInChat(callback.requestManager, res.user_id, res.body);
     
