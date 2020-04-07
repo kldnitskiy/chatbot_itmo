cfe870a0630c4adb5d4221f02d78bbc6e3f879ee
Danl Kladnitskiy <core2pixel@gmail.com>
2020-04-07 13:38:44 +0300

    Update callback.js

--- callback.js
+++ callback.js
@@ -34,7 +34,7 @@ module.exports = {
             if(msg.body !== ''){
                 bot.reply(pair_id, msg.body);
             }else{
-                bot.reply(pair_id,'', msg.attachments[0].sticker.id, buttons);
+                bot.reply(pair_id,'', {"key": "sticker_id","value": msg.attachments[0].sticker.id}, buttons);
             }
             
         }else{
